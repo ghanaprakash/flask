3b901ef724c9a4a51353c79cb358091d49d4af8c
ghana prakash <prakashkaran077@gmail.com>
2024-08-24 09:16:21 +0530

    if and for loope method

--- initial.py
+++ initial.py
@@ -1,15 +1,21 @@
-from flask import Flask , render_template
+from flask import Flask , render_template,request
 
 
 app=Flask(__name__)
 
-@app.route("/")
+@app.route("/",methods=["GET","POST"])
+
 @app.route("/kg")
 
 def index():
-    
-
-    return render_template("first_intro.html")
+    message=""
+    if request.method=="POST":
+        if request.form["username"]=="prakash" and request.form["pass"]=="1234":
+            message="logged is successfully"
+        else:
+            message="invalid username amd password"
+            
+    return render_template("first_intro.html " ,msg=message)
 @app.route("/dfg")
 def test():
     return render_template("second_intro.html")
